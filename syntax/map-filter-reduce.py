376a1657b70fae9cbef1974af444d7d98ce3e024
scottwdavidson <sdavi1010@gmail.com>
2023-10-11 09:42:59 +0100

    add syntax:filter

--- syntax/map-filter-reduce.py
+++ syntax/map-filter-reduce.py
@@ -18,3 +18,21 @@ def append_today_date(descriptor):
 dated_list_of_photo_names = list(map(append_today_date, list_of_photo_names))
 print(dated_list_of_photo_names)
 
+"""
+Filter:
+Similar to map(), the filter() higher-order function takes a function and an iterable as inputs. 
+The function in case needs to be of a Boolean nature, returning True/False values corresponding 
+to the filter conditions. As output, it returns a subset of the input data that meets the 
+conditions stipulated by the function. Its syntax is defined as follows:
+
+filter(function, iterable)
+"""
+
+def only_castles(descriptor):
+    CASTLE = "Castle"
+    return descriptor[0:len(CASTLE)] == CASTLE
+
+list_of_castles = list(filter(only_castles, list_of_photo_names))
+print("---- filtered castles ----")
+print(list_of_castles)
+print(only_castles("Castle-A"))
